b7a7153f0914d632aef105c0cfff8f796b9c4d5f
Igor Lima <igorlimadossantos.igorlimasan@gmail.com>
2017-07-05 21:08:44 -0300

    Alteração de um print

--- cliente_tcp.py
+++ cliente_tcp.py
@@ -4,7 +4,6 @@ PORT = 5000            # Porta que o Servidor esta
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 tcp.connect(dest)
-print('Para sair digite 0(zero)\n')
 print(tcp.recv(1024).decode(encoding='UTF-8')) #recebe o texto enviado pelo servidor
 
 
